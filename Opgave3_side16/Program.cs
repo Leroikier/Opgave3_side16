5eaeb3e3078e046ad5be8f111dac2b50fd2b1253
Alexander Roikier Møller <alex6308@elev.tec.dk>
2018-08-15 12:43:13 +0200

    <Færdigt resultat der bliver enten rundet op eller ned til de nærmeste hundrere>

---
+++ Opgave3_side16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave3_side16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input "decimaltal"
+            Console.WriteLine("Indtast decimaltal");
+            decimal decimaltal = decimal.Parse(Console.ReadLine());
+
+            //Input bliver rundet op til de nærmeste hundreder via "Math.Ceiling"
+            decimaltal = Math.Round(decimaltal / 100) * 100;
+
+            //Udskrivning af "decimaltal" + tekst
+            Console.WriteLine(decimaltal + " Tallet bliver rundet op til de nærmeste hundreder");
+
+            //Slut
+            Console.ReadKey();
+        }
+    }
+}
